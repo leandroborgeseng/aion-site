24c2bdd4288a8392ef471f0d1e96a2c5f1ac5644
Leandro Borges <leandro.borges@me.com>
2026-09-15 21:59:41 -0300

    Moderniza apresentação institucional da Aion nos três idiomas

--- src/app/(site)/pt/page.tsx
+++ src/app/(site)/pt/page.tsx
@@ -1,27 +1,6 @@
-import { Footer } from "@/components/Footer";
-import { HeroSection } from "@/components/HeroSection";
-import { Navbar } from "@/components/Navbar";
-import { SectionAutomacao } from "@/components/SectionAutomacao";
-import { SectionEngClinica } from "@/components/SectionEngClinica";
-import { SectionEngHospitalar } from "@/components/SectionEngHospitalar";
-import { SectionGestaoAtivos } from "@/components/SectionGestaoAtivos";
-import { ServicesGrid } from "@/components/ServicesGrid";
-import { TopBar } from "@/components/TopBar";
+import { HomePage } from "@/components/HomePage";
 import dict from "@/content/aion-pt.json";
 
-export default function PagePt() {
-  return (
-    <main>
-      <TopBar dict={dict.topbar} lang="pt" />
-      <Navbar dict={dict.nav} lang="pt" />
-      <HeroSection dict={dict.hero} lang="pt" />
-      <ServicesGrid dict={dict.services} lang="pt" />
-      <SectionEngClinica dict={dict.engClinica} />
-      <SectionGestaoAtivos dict={dict.gestaoAtivos} />
-      <SectionEngHospitalar dict={dict.engHospitalar} />
-      <SectionAutomacao dict={dict.automacao} />
-      <Footer dict={dict.footer} />
-    </main>
-  );
+export default function Page() {
+  return <HomePage dict={dict} lang="pt" />;
 }
-
